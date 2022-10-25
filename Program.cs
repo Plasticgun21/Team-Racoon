ec62f6cafdd05bd48f9769bba9f22b6e68dae39d
Pianonic <79938743+Pianonic@users.noreply.github.com>
2022-10-25 09:49:49 +0200

    Visual Studio Files

--- Program.cs
+++ Program.cs
@@ -7,8 +7,6 @@ namespace VociTrainerLA1200
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World2!");
-            Console.Write("Amongus");
         }
     }
 }
