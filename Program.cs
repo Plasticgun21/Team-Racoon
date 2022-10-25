579dbd1f3513a8f91051ae9b3903c1fa69371ff1
Pianonic <79938743+Pianonic@users.noreply.github.com>
2022-10-25 10:32:31 +0200

    VisualStudio

--- Program.cs
+++ Program.cs
@@ -12,7 +12,6 @@ namespace VociTrainerLA1200
         static void Main(string[] args)
         {
             Intro();
-            Console.WriteLine(Program.Einlesen);
 
         }
 
@@ -23,7 +22,6 @@ namespace VociTrainerLA1200
 
         static void Einlesen()
         {
-            
         }
     }
 }
