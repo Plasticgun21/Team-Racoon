f8027b0d6846db6354e2ab657a05d2349c9e3f84
Pianonic <79938743+Pianonic@users.noreply.github.com>
2022-10-25 09:48:18 +0200

    VS

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace VociTrainerLA1200
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World2!");
+            Console.Write("Amongus");
         }
     }
 }
