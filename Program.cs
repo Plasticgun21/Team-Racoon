91e3c87d06c84fb545aa9c1290cbd075cdb6c69c
Pianonic <79938743+Pianonic@users.noreply.github.com>
2022-11-01 11:51:44 +0100

    VisualStudio

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Voci_Trainer
     {
         static void Main(string[] args)
         {
-            Selector();
+           
             English();
         }
 
@@ -22,73 +22,144 @@ namespace Voci_Trainer
             
             string Antwort0;
             string Antwortf;
-            string Antwort;
+            string Antwort = "Deusch";
 
 
             Console.WriteLine("Wollen sie französische Wörter oder englische Wörter lernen?");
                 Antwort0 = Console.ReadLine();
             switch(Antwort0)
             {
-                case englisch: Console.WriteLine("Wollen sie mit Deutsch oder Englisch Antworten?");
-                    try
+                case "englisch":
                     {
-                        Antwort = Console.ReadLine();
+                        Console.WriteLine("Wollen sie mit Deutsch oder Englisch Antworten?");
+                        try
+                        {
+                            Antwort = Console.ReadLine();
 
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Falsche Eingabe.");
+                            //ApplicationException.Exit();
+                        }
+                        switch (Antwort)
+                        {
+
+                            case "deutsch":
+                                break;
+                            case "englisch":
+                                break;
+
+                            case "Deutsch":
+                                break;
+                            case "Englisch":
+                                break;
+                            default:
+                                throw new Exception();
+                                
+                        }
+                        break;
                     }
-                    catch
+
+                case "französisch":
+                    { Console.WriteLine("Wollen sie mit Deutsch oder Französisch Antworten?");
+                    
+                        try
+                        {
+                            Antwort = Console.ReadLine();
+
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Falsche Eingabe.");
+                            //ApplicationException.Exit();
+                        }
+                        switch (Antwort)
+                        {
+
+                            case "deutsch":
+                                break;
+                            case "französisch":
+                                break;
+
+                            case "Deutsch":
+                                break;
+                            case "Französisch":
+                                break;
+                            default:
+                                throw new Exception();
+                                
+                        }
+                        break;
+                    }
+                case "Französisch":
                     {
-                        Console.WriteLine("Falsche Eingabe.");
-                        ApplicationException.Exit();
+                        Console.WriteLine("Wollen sie mit Deutsch oder Französisch Antworten?");
+
+                        try
+                        {
+                            Antwort = Console.ReadLine();
+
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Falsche Eingabe.");
+                            //ApplicationException.Exit();
+                        }
+                        switch (Antwort)
+                        {
+
+                            case "deutsch":
+                                break;
+                            case "französisch":
+                                break;
+
+                            case "Deutsch":
+                                break;
+                            case "Französisch":
+                                break;
+                            default:
+                                throw new Exception();
+                                
+                        }
+                        break;
                     }
-                    switch (Antwort)
+                case "Englisch":
                     {
+                        Console.WriteLine("Wollen sie mit Deutsch oder Englisch Antworten?");
+                        try
+                        {
+                            Antwort = Console.ReadLine();
 
-                        case deutsch:
-                            break;
-                        case englisch:
-                            break;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Falsche Eingabe.");
+                            //ApplicationException.Exit();
+                        }
+                        switch (Antwort)
+                        {
 
-                        case Deutsch:
-                            break;
-                        case Englisch:
-                            break;
-                        default:
-                            throw new Exception();
-                            break;
+                            case "deutsch":
+                                break;
+                            case "englisch":
+                                break;
+
+                            case "Deutsch":
+                                break;
+                            case "Englisch":
+                                break;
+                            default:
+                                throw new Exception();
+                                
+                        }
                     }
+                    break;
             }
-            break;
+           
                
-            case französisch: Console.WriteLine("Wollen sie mit Deutsch oder Französisch Antworten?");
-            {
-                try
-                {
-                    Antwort = Console.ReadLine();
-
-                }
-                catch
-                {
-                    Console.WriteLine("Falsche Eingabe.");
-                    ApplicationException.Exit();
-                }
-                switch (Antwort)
-                {
-
-                    case deutsch:
-                        break;
-                    case französisch:
-                        break;
-
-                    case Deutsch:
-                        break;
-                    case Französisch:
-                        break;
-                    default:
-                        throw new Exception();
-                        break;
-                }
-                break;
-            }
+          
+            
         }
         
                
@@ -111,9 +182,15 @@ namespace Voci_Trainer
 
 
             int Count = 0;
-            int Durchlauf = 0;
+            string[] Temp = new string[4];
             foreach (string z in Zeilen)
             {
+                Temp = z.Split(';');
+                Englisch[Count] = Temp[0];
+                Deutsch[Count] = Temp[1];
+                Markieren[Count] = Temp[2];
+                System[Count] = Temp[3];
+                Count++;
 
             }
         
