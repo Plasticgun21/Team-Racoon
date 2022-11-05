b063ccc662bd61c5ad7ab31732c129b8df808b6a
Pianonic <79938743+Pianonic@users.noreply.github.com>
2022-11-05 20:56:09 +0100

    Visual Studio

--- Program.cs
+++ Program.cs
@@ -20,13 +20,13 @@ namespace Voci_Trainer
         static void Selector()
         {
             
-            string Antwort0;
-            string Antwortf;
+            
+            
             string Antwort = "Deusch";
 
 
             Console.WriteLine("Wollen sie französische Wörter oder englische Wörter lernen?");
-                Antwort0 = Console.ReadLine();
+            string Antwort0 = Console.ReadLine();
             switch(Antwort0)
             {
                 case "englisch":
@@ -157,21 +157,12 @@ namespace Voci_Trainer
                     break;
             }
            
-               
-          
             
         }
 
-
-
-
-
-
-
-
-
         static void English()
         {
+            bool DebugMode = true;
             string Sprache = "Englisch";
 
             string DateiPfad = @"C:\Users\nicla\Source\Repos\Team-Racoon\Excel\Deutsch Englisch.CSV";
@@ -201,34 +192,133 @@ namespace Voci_Trainer
                 //Console.WriteLine(z);
             }
 
-            bool Lernmodus = false;
+            bool Lernmodus = true;
 
             int RichitgCount=0;
             int FalseCount=0;
             Random random = new Random();
 
             
-            if (Lernmodus)
+            
+            if (Lernmodus == false)
             {
-                while (true)
+                Console.WriteLine("Wie viele Wörter möchtest du lernen?");
+                int Durchläufe = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                for (int i = 0; i < Durchläufe; i++)
                 {
                     int Zuf = random.Next(Zeilen.Length);
-                    Console.WriteLine($"Was ist das {Sprache}e Wort für {Deutsch[Zuf]}");
+                    Console.WriteLine($"  Was ist das {Sprache}e Wort für {Deutsch[Zuf]}");
                     string Antwort = Console.ReadLine();
 
                     if (string.Equals(Antwort, Englisch[Zuf], StringComparison.OrdinalIgnoreCase))
                     {
-                        Console.WriteLine("Richtig!");
+                        Console.WriteLine(" Richtig!");
                         RichitgCount++;
                     }
 
                     else
                     {
-                        Console.WriteLine($"Falsch! Richtig währe {Englisch[Zuf]}");
+                        Console.WriteLine($" Falsch! Richtig währe {Englisch[Zuf]}");
                         FalseCount++;
+                        Zeilen[Zuf] = $"{Deutsch[Zuf]};{Englisch[Zuf]};{Markieren[Zuf]};x";
                     }
                 }
+                Console.Clear();
+                File.WriteAllLines(DateiPfad, Zeilen);
+                Console.WriteLine($"Du hattest {RichitgCount} Richig und {FalseCount} Falsch");
             }
+
+            if(Lernmodus)
+            {
+                //Counter wird auf 0 gesetzt und es wird ein Array namens temp erstellt.
+                Count = 0;
+                int[] temp;
+
+                //Es wird im Array System gesucht, ob das zeichen "x" vorhanden ist wenn ja, wird der counter +1 gerechnet.
+                foreach (string Mar in System)
+                {
+                    if(Mar == "x")
+                    {
+                        Count++;
+                    }
+                    
+                }
+
+                //Nun wird der Counterwert dem Array zugewisen, der counter auf 0
+                //um ihn wieder zu verwenden und es wird noch ein zusätzlicher Counter erstellt.
+                temp = new int[Count];
+                Count = 0;
+                int Count2 = 0;
+
+                //Es wird zuerst im Array System gesucht, ob das zeichen "x" vorhanden ist wenn ja,
+                //wird der wert des counters in das Array temp eingeschrieben um bei den anderen arrays das Wort auszulesen.
+                foreach (string Mar in System)
+                {
+                    Count++;
+                    if (Mar=="x")
+                    {
+                        temp[Count2] = Count;
+                        Count2++;
+                    }
+                }
+
+                bool überprüfung = false;
+
+                while (true)
+                {
+                    //führe solange aus wie das array temp lang ist.
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        //überprüfen, ob alle die person alle wörter kennt.
+                        foreach (int te in temp)
+                        {
+                            if(te != -1)
+                            {
+                                break;
+                            }
+                        }
+                        //wenn im Array temp der wert -1 gefunden wird, soll es diesen überspringen.
+                        while (temp[i] == -1)
+                        {
+                            i++;
+                        }
+
+                        Console.WriteLine($"  Was ist das {Sprache}e Wort für {Deutsch[temp[i]]}");
+                        string Antwort = Console.ReadLine();
+
+                        if (string.Equals(Antwort, Englisch[temp[i]], StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine(" Richtig!");
+                            RichitgCount++;
+                            //wenn es richtig war im Array den wert mit -1 ersetzen, damit es überspringt wird.
+                            temp[i] = -1;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine($" Falsch! Richtig währe {Englisch[temp[i]]}");
+                            FalseCount++;
+
+                        }
+                        if (DebugMode)
+                        {
+                            foreach (int z in temp)
+                            {
+                                Console.WriteLine($" DEBUG {z} -_- {i}");
+                            }
+                        }
+                    }
+                    
+                }
+
+                foreach (int te in temp)
+                {
+                    //Console.WriteLine(te);
+                }
+            }
+            
         }
     }
 
